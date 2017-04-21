c27f1337e7324ea6554f6e223b868d13c3c8ea86
marpi <marpi@roygbi.ws>
2017-04-21 14:52:53 +0200

    Cleanup + Meta

--- js/third-party/Odeo.js
+++ js/third-party/Odeo.js
@@ -1,185 +1,209 @@
 // /ËˆÃ´dÄ“ËŒÅ/
 
-( function() {
+(function () {
 
-var AudioContext = window.AudioContext || window.webkitAudioContext;
+    var AudioContext = window.AudioContext || window.webkitAudioContext;
 
-function OdeoSoundCloudPlayer( id, odeo ) {
+    function OdeoSoundCloudPlayer(id, odeo) {
 
-	this.id = id;
-	this.odeo = odeo;
+        this.id = id;
+        this.odeo = odeo;
 
-	SC.initialize({
-		client_id: this.id
-	});
+        SC.initialize({
+            client_id: this.id
+        });
 
-	this.audio = document.createElement( 'audio' );
-	this.audio.loop = true;
-	this.audio.autoplay = true;
-	this.audio.crossOrigin = '';
+        this.audio = document.createElement('audio');
+        this.audio.loop = true;
+        this.audio.autoplay = true;
+        this.audio.crossOrigin = '';
 
-	this.songSource = this.odeo.context.createMediaElementSource( this.audio );
-	this.songSource.connect( this.odeo.analyser );
-	this.songSource.connect( this.odeo.context.destination );
+        this.songSource = this.odeo.context.createMediaElementSource(this.audio);
+        this.songSource.connect(this.odeo.analyser);
+        this.songSource.connect(this.odeo.context.destination);
 
-}
+    }
 
-OdeoSoundCloudPlayer.prototype.getSong = function( songURL ) {
+    OdeoSoundCloudPlayer.prototype.getSong = function (songURL) {
 
-	this.audio.play();
+        this.audio.play();
 
-	SC.resolve( songURL ).then( function( song ){
+        SC.resolve(songURL).then(function (song) {
 
-		console.log( song );
-		//songInfo.innerHTML = '<p><b><a href="' + song.permalink_url + '" >' + song.title + '</a> <a href="#" id="pauseBtn" >PAUSE</a></b><br/><a href="' + song.user.permalink_url + '">' + song.user.username + '</a></p>'
+            console.log(song);
+            //songInfo.innerHTML = '<p><b><a href="' + song.permalink_url + '" >' + song.title + '</a> <a href="#" id="pauseBtn" >PAUSE</a></b><br/><a href="' + song.user.permalink_url + '">' + song.user.username + '</a></p>'
 
-		this.audio.src = song.stream_url + "?client_id=" + this.id;
+            this.audio.src = song.stream_url + "?client_id=" + this.id;
 
-		this.songSource.connect( this.odeo.analyser );
-		this.songSource.connect( this.odeo.context.destination );
+            this.songSource.connect(this.odeo.analyser);
+            this.songSource.connect(this.odeo.context.destination);
 
-	}.bind( this ) );
+        }.bind(this));
 
-}
+    }
 
-OdeoSoundCloudPlayer.prototype.stop = function() {
+    OdeoSoundCloudPlayer.prototype.stop = function () {
 
-	this.audio.pause();
+        this.audio.pause();
 
-}
+    }
 
-function OdeoMediaPlayer( odeo ) {
+    function OdeoMediaPlayer(odeo) {
 
-	this.audioElement = document.createElement( 'audio' );
-	this.odeo = odeo;
+        this.audioElement = document.createElement('audio');
+        this.audioElement.controls = 'controls';
+        this.audioElement.style.position = "absolute";
+        this.audioElement.style.bottom = "0";
+        this.audioElement.style.width = "300px";
+        this.audioElement.style.left = window.innerWidth / 2 - 300 / 2 + "px";
+        document.body.appendChild(this.audioElement);
+        odeo.audioElement = this.audioElement
+        this.odeo = odeo;
 
-}
+        window.addEventListener("resize", onResize)
+    }
 
-OdeoMediaPlayer.prototype.play = function( src ) {
+    function onResize() {
+        if (this.odeo.audioElement)
+            this.odeo.audioElement.style.left = window.innerWidth / 2 - 300 / 2 + "px";
 
-	if( typeof src === 'string' ) {
-		this.audioElement.src = src;
-		this.audioElement.play();
-		this.audioSource = this.odeo.context.createMediaElementSource( this.audioElement );
-	} else {
-		this.audioSource = this.odeo.context.createMediaElementSource( src );
-	}
-	this.audioSource.connect( this.odeo.analyser );
-	this.audioSource.connect( this.odeo.context.destination );
+    }
 
-}
+    OdeoMediaPlayer.prototype.play = function (src) {
 
-function OdeoMicrophone( odeo ) {
+        if (typeof src === 'string') {
+            this.audioElement.src = src;
+            if (this.audioElement.canPlayType('audio/mpeg;')) {
+                //this.audioElement.type = 'audio/mpeg';
+                this.audioElement.src = src+'.mp3';
+            } else {
+                //this.audioElement.type = 'audio/ogg';
+                this.audioElement.src = src+'.ogg';
+            }
+            this.audioElement.play();
+            this.audioSource = this.odeo.context.createMediaElementSource(this.audioElement);
+        } else {
+            this.audioSource = this.odeo.context.createMediaElementSource(src);
+        }
+        this.audioSource.connect(this.odeo.analyser);
+        this.audioSource.connect(this.odeo.context.destination);
 
-	this.microphone = null;
-	this.odeo = odeo;
+    }
 
-}
+    function OdeoMicrophone(odeo) {
 
-OdeoMicrophone.prototype.play = function() {
+        this.microphone = null;
+        this.odeo = odeo;
 
-	if( navigator.getUserMedia ) {
+    }
 
-		navigator.getUserMedia( { audio: true }, function( stream ) {
+    OdeoMicrophone.prototype.play = function () {
 
-			this.microphone = this.odeo.context.createMediaStreamSource( stream );
-			this.microphone.connect( this.odeo.analyser );
+        if (navigator.getUserMedia) {
 
-		}.bind( this ),
-		function() {
+            navigator.getUserMedia({audio: true}, function (stream) {
 
-		} );
+                this.microphone = this.odeo.context.createMediaStreamSource(stream);
+                this.microphone.connect(this.odeo.analyser);
 
-	} else if( navigator.mediaDevices ) {
+            }.bind(this),
+                    function () {
 
-		navigator.mediaDevices.getUserMedia( { audio: true } ).then(function( stream ) {
+                    });
 
-			this.microphone = this.odeo.context.createMediaStreamSource( stream );
-			this.microphone.connect( this.odeo.analyser );
+        } else if (navigator.mediaDevices) {
 
-		}.bind( this ) ).catch(function(err) {
-		});
-	}
+            navigator.mediaDevices.getUserMedia({audio: true}).then(function (stream) {
 
-}
+                this.microphone = this.odeo.context.createMediaStreamSource(stream);
+                this.microphone.connect(this.odeo.analyser);
 
-OdeoMicrophone.prototype.stop = function() {
+            }.bind(this)).catch(function (err) {
+            });
+        }
 
-	this.microphone.disconnect( this.odeo.analyser );
+    }
 
-}
+    OdeoMicrophone.prototype.stop = function () {
 
-function Odeo( opts ){
+        this.microphone.disconnect(this.odeo.analyser);
 
-	this.options = opts || {};
+    }
 
-	this.context = new AudioContext();
-	this.analyser = this.context.createAnalyser();
-	this.analyser.fftSize = this.options.fftSize || 256;
-	this.frequencyData = new Uint8Array( this.analyser.frequencyBinCount );
+    function Odeo(opts) {
 
-	this.spectrumTexture = null;
+        this.options = opts || {};
 
-	this.soundCloudPlayer = null;
-	this.microphone = null;
-	this.media = null;
+        this.context = new AudioContext();
+        this.analyser = this.context.createAnalyser();
+        this.analyser.fftSize = this.options.fftSize || 256;
+        this.frequencyData = new Uint8Array(this.analyser.frequencyBinCount);
 
-}
+        this.spectrumTexture = null;
 
-Odeo.prototype.playMedia = function( src ) {
+        this.soundCloudPlayer = null;
+        this.microphone = null;
+        this.media = null;
 
-	if( !this.media ) this.media = new OdeoMediaPlayer( this );
-	this.media.play( src );
+    }
 
-}
+    Odeo.prototype.playMedia = function (src) {
 
-Odeo.prototype.useMicrophone = function() {
+        if (!this.media)
+            this.media = new OdeoMediaPlayer(this);
+        this.media.play(src);
 
-	if( !this.microphone ) this.microphone = new OdeoMicrophone( this );
-	this.microphone.play();
+    }
 
-}
+    Odeo.prototype.useMicrophone = function () {
 
-Odeo.prototype.stopUsingMicrophone = function() {
+        if (!this.microphone)
+            this.microphone = new OdeoMicrophone(this);
+        this.microphone.play();
 
-	if( !this.microphone ) return;
-	this.microphone.stop();
+    }
 
-}
+    Odeo.prototype.stopUsingMicrophone = function () {
 
-Odeo.prototype.playSoundCloud = function( url ) {
+        if (!this.microphone)
+            return;
+        this.microphone.stop();
 
-	if( !this.soundCloudPlayer ) this.soundCloudPlayer = new OdeoSoundCloudPlayer( this.options.soundCloudId, this );
-	this.soundCloudPlayer.getSong( url );
+    }
 
-}
+    Odeo.prototype.playSoundCloud = function (url) {
 
-Odeo.prototype.stopSoundCloud = function( url ) {
+        if (!this.soundCloudPlayer)
+            this.soundCloudPlayer = new OdeoSoundCloudPlayer(this.options.soundCloudId, this);
+        this.soundCloudPlayer.getSong(url);
 
-	if( !this.soundCloudPlayer ) return;
-	this.soundCloudPlayer.stop();
+    }
 
-}
+    Odeo.prototype.stopSoundCloud = function (url) {
 
-Odeo.prototype.getSpectrumTexture = function() {
+        if (!this.soundCloudPlayer)
+            return;
+        this.soundCloudPlayer.stop();
 
-	this.spectrumTexture = new THREE.DataTexture( this.frequencyData, 1 * this.frequencyData.length, 1, THREE.LuminanceFormat );
-	this.spectrumTexture.minFilter = THREE.NearestFilter;
-	this.spectrumTexture.magFilter = THREE.NearestFilter;
-	this.spectrumTexture.needsUpdate = true;
+    }
 
-	return this.spectrumTexture;
+    Odeo.prototype.getSpectrumTexture = function () {
 
-}
+        this.spectrumTexture = new THREE.DataTexture(this.frequencyData, 1 * this.frequencyData.length, 1, THREE.LuminanceFormat);
+        this.spectrumTexture.minFilter = THREE.NearestFilter;
+        this.spectrumTexture.magFilter = THREE.NearestFilter;
+        this.spectrumTexture.needsUpdate = true;
 
-Odeo.prototype.update = function() {
+        return this.spectrumTexture;
 
-	this.analyser.getByteFrequencyData( this.frequencyData );
-	if( this.spectrumTexture ) this.spectrumTexture.needsUpdate = true;
-	//kick.onUpdate();
+    }
 
-}
+    Odeo.prototype.update = function () {
+        this.analyser.getByteFrequencyData(this.frequencyData);
+        if (this.spectrumTexture)
+            this.spectrumTexture.needsUpdate = true;
+    }
 
-window.Odeo = Odeo;
+    window.Odeo = Odeo;
 
-} )();
+})();
